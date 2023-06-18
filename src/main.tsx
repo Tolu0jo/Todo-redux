2c6ea4ee8d79cf34f4b560e6d64d9b93d5c03454
ToluOjo <ojotolulpe137@gmail.com>
2023-06-18 03:46:44 +0100

    redux configuration

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import store from './store/store.ts' //encapsulate the html with data from store
-import {Provider} from "react-redux" // provides the state data to the entire applicatipn
+import {Provider} from "react-redux" // provides the state data to the entire application
+
+ 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Provider store={store}> {/* //pass the store as props to te provider */}
     <App />
+    </Provider>
   </React.StrictMode>,
 )
